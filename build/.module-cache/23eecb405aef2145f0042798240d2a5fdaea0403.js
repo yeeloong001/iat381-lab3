82c1d288ee90d0e52c61d024f4e82450d5a621f4
yeeloong <yeeloong.001@gmail.com>
2015-01-28 22:55:10 -0800

    initial react framework

---
+++ build/.module-cache/23eecb405aef2145f0042798240d2a5fdaea0403.js
@@ -0,0 +1,4 @@
+React.render(
+  React.createElement('h1', null, 'Hello, world1!'),
+  document.getElementById('example')
+);
---
+++ build/.module-cache/759be00defcf5c58275cca20fff44ed0848101e5.js
@@ -0,0 +1,26 @@
+React.render(
+  React.createElement('h1', null, 'Hello, world!'),
+  document.getElementById('example')
+);
+
+var ExampleApplication = React.createClass({displayName: "ExampleApplication",
+        render: function() {
+          var elapsed = Math.round(this.props.elapsed  / 100);
+          var seconds = elapsed / 10 + (elapsed % 10 ? '' : '.0' );
+          var message =
+            'React has been successfully running for ' + seconds + ' seconds.';
+
+          return React.DOM.p(null, message);
+        }
+      });
+
+      // Call React.createFactory instead of directly call ExampleApplication({...}) in React.render
+      var ExampleApplicationFactory = React.createFactory(ExampleApplication);
+
+      var start = new Date().getTime();
+      setInterval(function() {
+        React.render(
+          ExampleApplicationFactory({elapsed: new Date().getTime() - start}),
+          document.getElementById('container')
+        );
+      }, 50);
